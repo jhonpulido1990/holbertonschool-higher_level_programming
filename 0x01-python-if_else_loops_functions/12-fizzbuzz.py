0708bceac6d69fd6916b88ad183627949e6278c4
jhonpulido1990 <3825@holbertonschool.com>
2021-12-21 18:24:15 -0500

    fixed for

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
+    for i in range(1, 101):
         number = i
         if number % 15 == 0:
             print("FizzBuzz", end=' ')
