5aeefb14b834413db0f18eaf2e84da5e22eb3aff
jhonpulido1990 <3825@holbertonschool.com>
2021-12-21 13:09:06 -0500

    ordenize str

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+str = "and is less than 6 and not 0"
+str1 = "and is greater than 5"
 if number > 0:
     last_digit = number % 10
 if number < 0:
@@ -8,6 +10,6 @@ if number < 0:
 if last_digit == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
 elif last_digit < 6:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
+    print("Last digit of {:d} is {:d} ".format(number, last_digit), str)
 elif last_digit > 6:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
+    print("Last digit of {:d} is {:d} ".format(number, last_digit), str)
