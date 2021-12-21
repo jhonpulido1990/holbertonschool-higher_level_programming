ff68b91ebfcf3f931a15d9a6f7d5672f1a57cc3f
jhonpulido1990 <3825@holbertonschool.com>
2021-12-21 13:16:38 -0500

    fixed in conditional in if and else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,13 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 str = "and is less than 6 and not 0"
 str1 = "and is greater than 5"
-if number > 0:
+if number >= 0:
     last_digit = number % 10
-if number < 0:
+else:
     last_digit = number % -10
 if last_digit == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
-elif last_digit < 6:
+elif last_digit < 6 and last_digit != 0:
     print("Last digit of {:d} is {:d} ".format(number, last_digit), str)
-elif last_digit > 6:
+else:
     print("Last digit of {:d} is {:d} ".format(number, last_digit), str1)
