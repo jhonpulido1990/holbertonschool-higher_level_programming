8d6889b65f1cc0b493c7f22dd0206f5c358b855b
jhonpulido1990 <3825@holbertonschool.com>
2021-12-21 16:45:21 -0500

    correct ouput word

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     for w in str:
-        if ord(w) > 96 and ord(w) < 123:
-            character = ord(w) - 32
-            w = chr(character)
-        print(w, end='')
+        character = ord(w)
+        if character > 96 and character < 123:
+            new_character = chr(character - 32)
+        else:
+            new_character = w
+        print("{}".format(new_character), end="")
     print("")
