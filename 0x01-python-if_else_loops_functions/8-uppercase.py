d83bc0e2e9d23101107e7d3031d768c31f01f00a
jhonpulido1990 <3825@holbertonschool.com>
2021-12-21 16:30:49 -0500

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for w in str:
+        if ord(w) > 96 and ord(w) < 123:
+            character = ord(w) - 32
+            w = chr(character)
+        print(w, end='')
+    print("")
