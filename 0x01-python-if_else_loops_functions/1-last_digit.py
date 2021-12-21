36ee4dd9e49ff42ee0c7950eaf1f69fd0c16d74d
jhonpulido1990 <3825@holbertonschool.com>
2021-12-21 13:04:57 -0500

    fixed conditional

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,15 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 if number > 0:
     last_digit = number % 10
-    if last_digit <= 6 and last_digit > 0:
-        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
-    if last_digit > 6 and last_digit > 0:
-        print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
-    if last_digit == 0:
-        print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
 if number < 0:
-    last_digit = (number * -1) % 10
-    if last_digit == 0:
-        print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
-    if last_digit > 0:
-        print("Last digit of {:d} is -{:d} and is less than 6 and not 0".format(number, last_digit))
+    last_digit = number % -10
+if last_digit == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
+elif last_digit < 6:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
+elif last_digit > 6:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
