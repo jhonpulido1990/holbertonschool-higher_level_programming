c98df2bed665bd316a674f30baabca56ef4b816f
jhonpulido1990 <3825@holbertonschool.com>
2021-12-23 15:32:41 -0500

    change output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,9 @@ if __name__ == "__main__":
         print("{} arguments.".format((len(sys.argv)-1)))
     elif (len(sys.argv)-1) == 1:
         print("{} argument:".format((len(sys.argv)-1)))
-        print("{} :".format(value + 1), str(sys.argv[1]))
     else:
         print("{} arguments:".format((len(sys.argv)-1)))
-        for w in sys.argv:
-            value = value + 1
-            if (len(sys.argv)-1) >= value:
-                print("{} :".format(value), sys.argv[value])
+    for w in sys.argv:
+        value = value + 1
+        if (len(sys.argv)-1) >= value:
+            print("{} :".format(value), sys.argv[value])
