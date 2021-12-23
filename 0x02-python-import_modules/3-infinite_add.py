cee7e10c10fd88ced98543db7355b286cb8b8cf8
jhonpulido1990 <3825@holbertonschool.com>
2021-12-23 16:20:05 -0500

    fixed conditional

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,13 +3,8 @@ if __name__ == "__main__":
     import sys
     value = 0
     f = 0
-    if (len(sys.argv)-1) == 0:
-        print(0)
-    elif (len(sys.argv)-1) == 1:
-        print(sys.argv[value + 1])
-    elif (len(sys.argv)-1) > 1:
-        for w in sys.argv:
-            value = value + 1
-            if (len(sys.argv)-1) >= value:
-                f = f + int(sys.argv[value])
-print("{:d}".format(f))
+    for w in sys.argv:
+        value = value + 1
+        if (len(sys.argv)-1) >= value:
+            f = f + int(sys.argv[value])
+    print("{:d}".format(f))
