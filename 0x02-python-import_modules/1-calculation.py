79bafc58cad1bf825c3db73f133b15c8590cd3cb
jhonpulido1990 <3825@holbertonschool.com>
2021-12-23 12:19:46 -0500

    corret uotput

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 from calculator_1 import add, div, sub, mul
 a = 10
 b = 5
-print('{} + {} = {}'.format(a, b, add(a, b)))
-print('{} - {} = {}'.format(a, b, sub(a, b)))
-print('{} * {} = {}'.format(a, b, mul(a, b)))
-print('{} / {} = {}'.format(a, b, div(a, b)))
+print(a, '+', b, '=', add(a, b))
+print(a, '+', b, '=', sub(a, b))
+print(a, '+', b, '=', mul(a, b))
+print(a, '+', b, '=', div(a, b))
