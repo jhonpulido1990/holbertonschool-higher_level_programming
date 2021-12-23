9deff0ecb1c9a581da378139358e1e7df91945d4
jhonpulido1990 <3825@holbertonschool.com>
2021-12-23 15:12:15 -0500

    add . and : in print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     import sys
     value = 0
     if (len(sys.argv)-1) == 0:
-        print("{} arguments".format((len(sys.argv)-1)))
+        print("{} arguments.".format((len(sys.argv)-1)))
     elif (len(sys.argv)-1) == 1:
-        print("{} argument".format((len(sys.argv)-1)))
+        print("{} argument:".format((len(sys.argv)-1)))
         print("{} :".format(len(sys.argv[1])), str(sys.argv[1]))
     else:
-        print("{} arguments".format((len(sys.argv)-1)))
+        print("{} arguments:".format((len(sys.argv)-1)))
         for w in sys.argv:
             value = value + 1
             if (len(sys.argv)-1) >= value:
