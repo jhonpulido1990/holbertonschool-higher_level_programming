142be6739c15866b6d93c039d06aab608838b754
jhonpulido1990 <3825@holbertonschool.com>
2021-12-23 15:15:28 -0500

    fixed secnd conditional statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("{} arguments.".format((len(sys.argv)-1)))
     elif (len(sys.argv)-1) == 1:
         print("{} argument:".format((len(sys.argv)-1)))
-        print("{} :".format(len(sys.argv[1])), str(sys.argv[1]))
+        print("{} :".format(value + 1), str(sys.argv[1]))
     else:
         print("{} arguments:".format((len(sys.argv)-1)))
         for w in sys.argv:
