ab4fbfd979d92d369c259b0c67ef6a76f0faccbb
jhonpulido1990 <3825@holbertonschool.com>
2022-01-03 17:16:07 -0500

    change function sum

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    res = (0, 0)
     if len(tuple_b) == 0:
         tuple_b = (0, 0)
     elif len(tuple_b) == 1:
@@ -8,5 +9,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (0, 0)
     elif len(tuple_a) == 1:
         tuple_a = (tuple_a[0], 0)
-    res = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    res = ((tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
     return res
