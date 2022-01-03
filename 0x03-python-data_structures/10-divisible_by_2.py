e1e109662feaf533f50348e4a000667692b20e32
jhonpulido1990 <3825@holbertonschool.com>
2022-01-03 18:38:36 -0500

    fixed create new list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,8 @@ def divisible_by_2(my_list=[]):
     new_list =[]
     for i in my_list:
         if i % 2 == 0:
-            new_list.append(True)
+            condition = True
         else:
-            new_list.append(False)
+            condition = False
+        new_list.append(condition)
     return new_list
