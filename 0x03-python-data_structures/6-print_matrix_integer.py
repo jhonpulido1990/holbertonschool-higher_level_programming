7bfda138f11acc76971ada6d97c674af9fd4fcb0
jhonpulido1990 <3825@holbertonschool.com>
2022-01-03 13:26:15 -0500

    correct output matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print ('{}'.format(row))
-
+    for i in range(len(matrix)):
+        print(*matrix[i], end='\n')
