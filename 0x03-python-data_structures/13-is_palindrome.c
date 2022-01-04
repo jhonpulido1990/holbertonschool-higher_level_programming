310b6a10065c5f02beba149a0a5fc9f0a26e6ce7
jhonpulido1990 <3825@holbertonschool.com>
2022-01-04 17:43:34 -0500

    delecte malloc and one loop

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,29 +10,21 @@ int is_palindrome(listint_t **head)
 {
 	int i = 0, a = 0;
 	listint_t *h = NULL;
-	char *args;
+	char args[1024];
 
-	h = *head;
-	for (i = 0; h; i++)
-		h = h->next;
-	a = i;
-	args = malloc((i + 1) * sizeof(char *));
-	if (args == NULL)
-		return (0);
 	h = *head;
 	for (i = 0; h; i++)
 	{
 		args[i] = h->n;
 		h = h->next;
 	}
+	a = i;
 	for (i = 0; i <= a/2; i++)
 	{
 		if (args[i] != args[a - (i + 1)])
 		{
-			free(args);
 			return (0);
 		}
 	}
-	free(args);
 	return (1);
 }
