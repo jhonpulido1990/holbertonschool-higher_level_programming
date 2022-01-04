17082e92c6b50a8e4133442a6e506bf69dbef693
jhonpulido1990 <3825@holbertonschool.com>
2022-01-04 17:01:20 -0500

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @brief
+ *
+ */
+int is_palindrome(listint_t **head)
+{
+	int i = 0, a = 0;
+	listint_t *h = NULL;
+	char *args;
+
+	h = *head;
+	for (i = 0; h; i++)
+		h = h->next;
+	a = i;
+	args = malloc((i + 1) * sizeof(char *));
+	if (args == NULL)
+		return (0);
+	h = *head;
+	for (i = 0; h; i++)
+	{
+		args[i] = h->n;
+		h = h->next;
+	}
+	for (i = 0; i <= a/2; i++)
+	{
+		if (args[i] != args[a - (i + 1)])
+			return (0);
+	}
+	
+	return (1);
+}
