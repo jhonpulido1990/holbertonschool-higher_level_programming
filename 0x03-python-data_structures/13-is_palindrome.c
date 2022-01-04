4ce00de5ba1b07767694b34bf1d8d1b8d699e00f
jhonpulido1990 <3825@holbertonschool.com>
2022-01-04 17:47:27 -0500

    change output

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,15 +10,14 @@ int is_palindrome(listint_t **head)
 {
 	int i = 0, a = 0;
 	listint_t *h = NULL;
-	char args[1024];
+	char args[4024];
 
 	h = *head;
-	for (i = 0; h; i++)
+	for (i = 0; h; i++,a++)
 	{
 		args[i] = h->n;
 		h = h->next;
 	}
-	a = i;
 	for (i = 0; i <= a/2; i++)
 	{
 		if (args[i] != args[a - (i + 1)])
