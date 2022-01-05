c9e18a1961ac4541aa65a65dc1589679f4bc0719
jhonpulido1990 <3825@holbertonschool.com>
2022-01-05 14:24:53 -0500

    add space in function print

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     order_dict = sorted(a_dictionary.items())
     for i in range(len(order_dict)):
-        print(order_dict[i][0] + ":",order_dict[i][1])
+        print(order_dict[i][0] + ":", order_dict[i][1])
