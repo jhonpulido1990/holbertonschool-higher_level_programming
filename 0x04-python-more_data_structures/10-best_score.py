399227920b62621e90852c572487b3a540cbb59f
jhonpulido1990 <3825@holbertonschool.com>
2022-01-05 17:20:43 -0500

    correct condition and modifity return

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
-        return None
-    else:
-        new_dictionary = max(a_dictionary)
-        return new_dictionary
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get) 
