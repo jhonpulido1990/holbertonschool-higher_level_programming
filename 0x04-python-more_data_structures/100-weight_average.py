4198fbd1a8bfbed9fd4a80daa69df90469caf311
jhonpulido1990 <3825@holbertonschool.com>
2022-01-05 22:15:29 -0500

    correct if my list is empty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == None:
+    if my_list:
+        a = []
+        b = []
+        for i in range(len(my_list)):
+            a.append(my_list[i][0] * my_list[i][1])
+            b.append(my_list[i][1])
+        result = sum(a)/sum(b)
+        return result
+    else:
         return 0
-    a = []
-    b = []
-    for i in range(len(my_list)):
-        a.append(my_list[i][0] * my_list[i][1])
-        b.append(my_list[i][1])
-    result = sum(a)/sum(b)
-    return result
