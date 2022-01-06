60f82d1ff6ad3dbe2efbe36bdeb4d145797380fa
jhonpulido1990 <3825@holbertonschool.com>
2022-01-05 22:10:17 -0500

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    a = []
+    b = []
+    for i in range(len(my_list)):
+        a.append(my_list[i][0] * my_list[i][1])
+        b.append(my_list[i][1])
+    result = sum(a)/sum(b)
+    return result
