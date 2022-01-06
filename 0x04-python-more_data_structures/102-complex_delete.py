2a9d55f47480fc85bfdc9036de30c8fcccdc072f
jhonpulido1990 <3825@holbertonschool.com>
2022-01-05 23:49:12 -0500

    task 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    copy_dict =  a_dictionary.copy()
+    for i in copy_dict.keys():
+        if value == copy_dict.get(i):
+            a_dictionary.pop(i, None)
+    return a_dictionary
