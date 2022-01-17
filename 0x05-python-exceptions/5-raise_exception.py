51195d077031aa9d9b768385bf8753676ba86f4e
jhonpulido1990 <3825@holbertonschool.com>
2022-01-17 17:22:50 -0500

    correct funtion

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-from logging import exception
-
-
 def raise_exception():
     try:
         raise_exception()
-    except exception as ex:
-        pass
+    except:
+        raise raise_exception
