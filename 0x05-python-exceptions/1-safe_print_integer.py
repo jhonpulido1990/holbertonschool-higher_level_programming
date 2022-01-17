a49bb8e9b6bb6219278487e1222ab61b5dd9c895
jhonpulido1990 <3825@holbertonschool.com>
2022-01-17 12:08:41 -0500

    fixed change funtion

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if (type(value) == int):
+        if int(value):
             print("{:d}".format(value))
             return True
     except:
