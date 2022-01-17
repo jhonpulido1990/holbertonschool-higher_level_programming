8d91f707ada1ccdb83fef948abad63245d59ad88
jhonpulido1990 <3825@holbertonschool.com>
2022-01-17 14:45:05 -0500

    task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from copy import copy
+from pickle import APPEND
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    lent = 0
+    resuk = []
+    while(lent < list_length):
+        try:
+            if (my_list_1[lent] and my_list_2[lent]):
+                try:
+                    if(int(my_list_1[lent]) and int(my_list_2[lent])):
+                        try:
+                            if(my_list_1[lent] != 0 or my_list_2[lent] != 0):
+                                resuk.append(my_list_1[lent] / my_list_2[lent])
+                        except:
+                            print("division by 0")
+                            resuk.append(0)
+                except:
+                    print("wrong type")
+                    resuk.append(0)
+        except:
+            print("out of range")
+            resuk.append(0)
+        lent += 1
+    return resuk
