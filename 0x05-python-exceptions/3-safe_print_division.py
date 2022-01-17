291b6fd29dbf6a1c130a9cd679f89680be31de24
jhonpulido1990 <3825@holbertonschool.com>
2022-01-17 12:56:44 -0500

    correct fixed ouput

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from unittest import result
+
+
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+        return result
+    except:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
