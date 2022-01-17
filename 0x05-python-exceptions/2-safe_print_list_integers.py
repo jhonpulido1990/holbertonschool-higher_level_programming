af63e41279345217b43345858a5f88158aae680e
jhonpulido1990 <3825@holbertonschool.com>
2022-01-17 12:33:13 -0500

    correct task 2 count

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,12 +4,11 @@ def safe_print_list_integers(my_list=[], x=0):
     b = 0
     while (x > a):
         try:
-            if (a) < x:
-                if type(my_list[a]) == int:
-                    print("{}".format(my_list[a]), end='')
-                    b = b + 1
-                a = a + 1
+            if int(my_list[a]):
+                print("{}".format(my_list[a]), end='')
+                b = b + 1
         except (ValueError, TypeError):
             pass
+        a = a + 1
     print()
     return b
