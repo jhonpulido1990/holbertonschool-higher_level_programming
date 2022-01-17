be8adcfeefd1237f5e0728837b09ed7edb09a8cb
jhonpulido1990 <3825@holbertonschool.com>
2022-01-17 12:19:35 -0500

    add in new line

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         if int(value):
-            print("{:d}".format(value))
+            print("{:d}".format(value), end='\n')
             return True
     except:
         return False
