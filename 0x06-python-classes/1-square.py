b87e8f5e0c5f405ba7583c6eab1ea1155145adea
jhonpulido1990 <3825@holbertonschool.com>
2022-01-18 16:34:50 -0500

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''create class'''
+
+
 class Square:
+    '''definition of class'''
     def __init__(self, size) -> None:
+        '''mode of private'''
         self.__size = size
