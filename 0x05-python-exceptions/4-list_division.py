2ae948c1d322109b0bc7b01a117cc1e8169e186f
jhonpulido1990 <3825@holbertonschool.com>
2022-01-17 19:09:03 -0500

    add cariable when save data

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     lent = 0
+    tmp = 0
     resuk = []
     while(lent < list_length):
         try:
-            resuk.append(my_list_1[lent] / my_list_2[lent])
+            tmp = my_list_1[lent] / my_list_2[lent]
         except(IndexError):
             print("out of range")
-            resuk.append(0)
+            tmp = 0
         except (TypeError):
             print("wrong type")
-            resuk.append(0)
+            tmp = 0
         except(ZeroDivisionError):
             print("division by 0")
-            resuk.append(0)
+            tmp = 0
         lent += 1
+        resuk.append(tmp)
     return resuk
