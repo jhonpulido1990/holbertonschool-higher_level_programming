ea882c02033853c5f6f9c353087286cbff94301b
jhonpulido1990 <3825@holbertonschool.com>
2022-01-18 18:33:08 -0500

    delecte space between funtions

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,7 +6,7 @@ class Square:
     '''create definition'''
     def __init__(self, size=0):
         self.__size = size
-    
+
     @property
     def size(self):
         return self.__size
@@ -19,6 +19,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     def area(self):
         return self.__size ** 2
