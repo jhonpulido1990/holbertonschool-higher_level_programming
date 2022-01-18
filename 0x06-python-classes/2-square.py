97f626f896f7c3015f09e1088ebec8834e42dda7
jhonpulido1990 <3825@holbertonschool.com>
2022-01-18 17:32:14 -0500

    task correct ouput

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
+'''create class'''
 
 class Square:
+    '''create definition'''
     def __init__(self, size=0):
-            self.size = size
-
-    @property
-    def size(self):
-        return self._size
-
-    @size.setter
-    def size(self, size):
-        self._size = size
-        if(type(size) != int):
+        self.__size = size
+        if (type(size) != int):
             raise TypeError("size must be an integer")
         elif(size < 0):
             raise ValueError("size must be >= 0")
+
