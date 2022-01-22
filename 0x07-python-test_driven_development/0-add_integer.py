d60cd5766afdd66ea62b46fde98c66ade803d07b
jhonpulido1990 <3825@holbertonschool.com>
2022-01-22 18:15:41 -0500

    fixed content and code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-'''create funtion add'''
+'''This function allows 
+you to add two integers or floats and its 
+result is an integer.
+add_integer(1, 3)
+4 '''
 
 
 def add_integer(a, b=98):
-    '''add'''
-    if not isinstance(a, int) and not isinstance(a, float):
+    '''in this function it is evaluated
+    if the variables are 
+    integers, floats'''
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
