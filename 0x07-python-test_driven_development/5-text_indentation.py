ac09b5cdb69b93fcb8543dbcd765ea8c6a855a5f
jhonpulido1990 <3825@holbertonschool.com>
2022-01-23 17:57:05 -0500

    add fixed code output

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 def text_indentation(text):
     strinspace = ""
+    new_space = ""
     if isinstance(text, str):
         text = text.strip()
         text = " ".join(text.split())
         for i in text:
             if '.' in i:
-                strinspace +=  '.\n\n\n'
+                strinspace += '.    '
             elif '?' in i:
-                strinspace += '?\n\n\n'
+                strinspace += '?    '
             elif ':' in i:
-                strinspace += ':\n\n\n'
+                strinspace += ':    '
             else:
                 strinspace += i
-        new_string = strinspace.replace('\n ', '')
-        
-        print(new_string)
+        strinspace += ' '
+        strinspace = " ".join(strinspace.split())
+        for i in strinspace:
+            if '.' in i:
+                new_space += '.\n\n\n'
+            elif '?' in i:
+                new_space += '?\n\n\n'
+            elif ':' in i:
+                new_space += ':\n\n\n'
+            else:
+                new_space += i
+        new_string = new_space.replace('\n ', '')    
+        print(new_string, end='')
     else:
         raise TypeError("text must be a string")
