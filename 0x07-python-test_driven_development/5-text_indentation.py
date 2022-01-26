7da381645c0d048979f884ce8762833545a6f7ca
jhonpulido1990 <3825@holbertonschool.com>
2022-01-25 19:24:27 -0500

    task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
+'''create funtion that identation
+each line of text
+
+example:
+use text_ identation(text to identation)'''
+
+
 def text_indentation(text):
+    '''funtion that concat string and
+    add line break and delecte space repeat
+    create a new string and save on it'''
     strinspace = ""
     new_space = ""
     if isinstance(text, str):
-        text = text.strip()
-        text = " ".join(text.split())
-        for i in text:
-            if '.' in i:
-                strinspace += '.    '
-            elif '?' in i:
-                strinspace += '?    '
-            elif ':' in i:
-                strinspace += ':    '
-            else:
-                strinspace += i
-        strinspace += ' '
-        strinspace = " ".join(strinspace.split())
-        for i in strinspace:
-            if '.' in i:
-                new_space += '.\n\n\n'
-            elif '?' in i:
-                new_space += '?\n\n\n'
-            elif ':' in i:
-                new_space += ':\n\n\n'
-            else:
-                new_space += i
-        new_string = new_space.replace('\n ', '')    
-        print(new_string, end='')
+        for i in ".?:":
+            text = text.replace(i, i + '\n\n')
+        strinspace = [charact.strip(' ') for charact in text.split("\n")]
+        new_space = "\n".join(strinspace)
+        print(new_space, end='')
     else:
         raise TypeError("text must be a string")
