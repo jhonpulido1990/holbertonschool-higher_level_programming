56cfbf2bb096ea0629dd49f5f2c0dc05743739c3
jhonpulido1990 <3825@holbertonschool.com>
2022-01-31 22:50:22 -0500

    correction subclass

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,25 +4,15 @@ Write a class BaseGeometry (based on 6-base_geometry.py).
 Public instance method: def area(self): that raises an
 Exception with the message area() is not implemented
 '''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
+class Rectangle(BaseGeometry):
     '''
     Write a class BaseGeometry
     (based on 6-base_geometry.py)'''
     def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
-        self.__heigth = height
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not type(value) is int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        if type(self.__width) is int and self.__width > 0:
-            pass
-        if type(self.__heigth) is int and self.__heigth > 0:
-            pass
+        self.__height = height
