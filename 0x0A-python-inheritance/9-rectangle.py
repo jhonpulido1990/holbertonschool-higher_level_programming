cacddbc9938a3c35abe6d560f21f52878895faa2
jhonpulido1990 <3825@holbertonschool.com>
2022-01-31 23:14:09 -0500

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Write a class BaseGeometry (based on 6-base_geometry.py).
+Public instance method: def area(self): that raises an
+Exception with the message area() is not implemented
+'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''
+    Write a class BaseGeometry
+    (based on 6-base_geometry.py)'''
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
+    def area(self):
+        return self.__width * self.__height
