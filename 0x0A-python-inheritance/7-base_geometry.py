1a5074493430d776076333cd792f3ccf7aa18c95
jhonpulido1990 <3825@holbertonschool.com>
2022-01-31 22:08:31 -0500

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Write a class BaseGeometry (based on 6-base_geometry.py).
+Public instance method: def area(self): that raises an
+Exception with the message area() is not implemented
+'''
+
+
+class BaseGeometry:
+    '''Write a class BaseGeometry (based on 6-base_geometry.py).
+    Public instance method: def area(self): that raises an
+    Exception with the message area() is not implemented'''
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
