aa799fc243cbede44a8906cc62d7a6bf54dea5dd
jhonpulido1990 <3825@holbertonschool.com>
2022-01-31 19:06:33 -0500

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+
+Write a class MyList that inherits from list
+
+'''
+
+class MyList(list):
+    '''Write a class MyList that inherits from list'''
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        numb = sorted(self.copy())
+        print(numb)
