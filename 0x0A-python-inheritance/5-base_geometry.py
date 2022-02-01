8fc4a7bf3b535288f99977c2133545614d2e7d56
jhonpulido1990 <3825@holbertonschool.com>
2022-01-31 21:52:19 -0500

    task

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Write an empty class BaseGeometry
+
+You are not allowed to import any module
+'''
+
+
+class BaseGeometry:
+    '''
+    Write an empty class BaseGeometry
+    '''
+    pass
