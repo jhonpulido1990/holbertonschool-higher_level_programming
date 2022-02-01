769de2f0e7032773abf428c79e3dc132e1521ff3
jhonpulido1990 <3825@holbertonschool.com>
2022-01-31 22:00:49 -0500

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Write a class
+BaseGeometry (based
+on 5-base_geometry.py).
+'''
+
+
+class BaseGeometry:
+    '''Write a class
+    BaseGeometry (based
+    on 5-base_geometry.py)'''
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
