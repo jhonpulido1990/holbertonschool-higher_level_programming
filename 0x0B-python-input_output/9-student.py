22dd95eebd47dee6d9cbe564bebe1c6e4062a317
jhonpulido1990 <3825@holbertonschool.com>
2022-02-01 22:11:15 -0500

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Write a class Student that
+
+defines a student by:
+'''
+
+
+class Student:
+    '''Write a class Student
+    that defines a student by:
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(obj):
+        return obj.__dict__
