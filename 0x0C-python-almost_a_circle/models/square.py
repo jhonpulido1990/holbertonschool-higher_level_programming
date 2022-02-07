5c7586ed5cd12f38c3bd9b559a0f3c817e403176
jhonpulido1990 <3825@holbertonschool.com>
2022-02-07 14:31:26 -0500

    add all comments square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,23 +14,38 @@ class Square(Rectangle):
     Class Square inherits from Rectangle
     '''
     def __init__(self, size, x=0, y=0, id=None):
+        '''
+        define of constructor
+        '''
         super().__init__(size, size, x, y, id)
         self.size = size
 
     @property
     def size(self):
+        '''
+        property definition size
+        '''
         return self.width
 
     @size.setter
     def size(self, value):
+        '''
+        method that sets the value of a property width
+        '''
         self.width = value
         self.height = value
 
     def __str__(self):
+        '''
+        print date
+        '''
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.size)
 
     def update(self, *args, **kwargs):
+        '''
+        data update
+        '''
         attr = ["id", "size", "x", "y"]
         if args:
             for i in range(len(args)):
@@ -42,6 +57,9 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
+        '''
+        create dictionary
+        '''
         dictio = {}
         dictio.update(id=self.id, size=self.size, x=self.x, y=self.y)
         return dictio
