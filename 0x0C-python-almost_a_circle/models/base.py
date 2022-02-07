df8afa1f2608363f5c43c17c46592c973988c8be
jhonpulido1990 <3825@holbertonschool.com>
2022-02-07 12:03:53 -0500

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+'''Write the first class Base:
+Create a folder named models with
+an empty file __init__.py inside -
+with this file, the folder will become
+a Python package'''
+import json
+
+
+class Base:
+    '''
+    Create a file named models/base.py
+    '''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        namefile = "{}.json".format(cls.__name__)
+        lista = []
+        if list_objs is not None:
+            for i in list_objs:
+                lista.append(cls.to_dictionary(i))
+        with open(namefile, 'w') as f:
+            f.write(cls.to_json_string(lista))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string or len(json_string) != 0:
+            return json.loads(json_string)
+        return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 2)
+        if cls.__name__ == "Square":
+            dummy = cls(3)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        listing = []
+        namefile = "{}.json".format(cls.__name__)
+        try:
+            with open(namefile, 'r') as f:
+                diction = cls.from_json_string(f.read())
+            for instance in diction:
+                listing.append(cls.create(**instance))
+            return listing
+        except FileNotFoundError:
+            pass
