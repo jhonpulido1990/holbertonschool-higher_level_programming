ac05b4c5b06defeea84c9cd9f68aed0cd8625ee4
jhonpulido1990 <3825@holbertonschool.com>
2022-03-15 11:35:43 -0500

    change all code

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,2 @@
 -- lists all records of the table second_table
-SELECT score, COUNT(score) 
-AS number 
-FROM second_table 
-GROUP BY score ORDER BY number DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
