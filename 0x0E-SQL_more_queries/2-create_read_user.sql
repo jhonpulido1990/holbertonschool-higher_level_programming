dad050951deeaa8e5e72e43a4783a50b797c9309
jhonpulido1990 <3825@holbertonschool.com>
2022-03-16 13:43:41 -0500

    task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Write a script that creates the database hbtn_0d_2 and the user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL SELECT ON * . * TO 'user_0d_1'@'localhost';
+CREATE DATABASES IF NOT EXISTS hbtn_0d_2;
