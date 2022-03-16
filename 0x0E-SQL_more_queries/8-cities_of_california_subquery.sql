5b466343774c8459a978ea26fb1ec4b829478b61
jhonpulido1990 <3825@holbertonschool.com>
2022-03-16 17:55:59 -0500

    change all code

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT id, name FROM cities WHERE stated_id = 1 ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE stated_id = 1 ORDER BY id ASC;
