ad52f2d11cb635663078b5540d50ca93f13e7f54
jhonpulido1990 <3825@holbertonschool.com>
2022-03-16 16:22:44 -0500

    cahnge reference table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL UNIQUE AUTO_INCREMENT,
-    state_id NOT NULL FOREING KEY (state_id) REFERENCE hbtn_0d_usa (id),
+    state_id NOT NULL FOREING KEY (state_id) REFERENCE states (id),
     name VARCHAR(256) NOT NULL
 );
