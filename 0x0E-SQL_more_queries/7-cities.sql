b1dd0d26697f9ba43de1c6e17547489738cc51f6
jhonpulido1990 <3825@holbertonschool.com>
2022-03-16 17:07:59 -0500

    change code in all

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL UNIQUE AUTO_INCREMENT,
-    state_id NOT NULL,
-    FOREING KEY (state_id) REFERENCE states (id),
+    state_id int NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
