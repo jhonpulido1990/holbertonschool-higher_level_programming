62365b32d00a05f1bf1056eda838df3353ca485c
jhonpulido1990 <3825@holbertonschool.com>
2022-03-17 13:00:09 -0500

    correctio add order asc

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,2 @@
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.genre_id;
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server:
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
