88ef8bee4263eb951a56504cff205c5fcbcea7c6
jhonpulido1990 <3825@holbertonschool.com>
2022-04-04 13:55:48 -0500

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let i = 0;
+const args = process.argv;
+if (args[2] === undefined) {
+  console.log('Missing number of occurrences');
+}
+a = parseInt(args[2]);
+while (i < args[2]) {
+  console.log('C is fun');
+  i++;
+}
