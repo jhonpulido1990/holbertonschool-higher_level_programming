426109dfcb452ab761e26a41bd887f6e0e8fd8c7
jhonpulido1990 <3825@holbertonschool.com>
2022-04-04 14:01:14 -0500

    delete variable var

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,8 +5,7 @@ const args = process.argv;
 if (args[2] === undefined || parseInt(args[2] != 'number')) {
   console.log('Missing number of occurrences');
 }
-a = parseInt(args[2]);
-while (i < args[2]) {
+while (i < parseInt(args[2])) {
   console.log('C is fun');
   i++;
 }
