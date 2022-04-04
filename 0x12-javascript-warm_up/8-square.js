d4973a3278a0a38e4fc287fd0788e61c40f9be80
jhonpulido1990 <3825@holbertonschool.com>
2022-04-04 14:43:41 -0500

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv;
+if (isNaN(parseInt(args[2]))) {
+  console.log('Missing size');
+}
+let i = 0;
+
+while (i < parseInt(args[2])) {
+  charact = 'X'.repeat(args[2]);
+  console.log(charact);
+  i++;
+}
