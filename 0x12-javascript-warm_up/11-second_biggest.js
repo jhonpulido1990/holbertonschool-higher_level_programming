5c5109bb5326d174d9c73d440812e822933c16cb
jhonpulido1990 <3825@holbertonschool.com>
2022-04-04 16:44:03 -0500

    add else

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,13 @@ const args = process.argv;
 let i = 2;
 if (args.length < 4) {
   console.log('0');
+} else {
+  const array = [];
+  while (i < args.length) {
+    array.push(parseInt(args[i]));
+    i++;
+  }
+  array.sort();
+  i = array.length - 2;
+  console.log(array[i]);
 }
-const array = [];
-while (i < args.length) {
-  array.push(parseInt(args[i]));
-  i++;
-}
-array.sort();
-i = array.length - 2;
-console.log(array[i]);
