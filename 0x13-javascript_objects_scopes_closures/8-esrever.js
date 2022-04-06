bd96e1849f1b0dab205e03779d1362dc44403840
jhonpulido1990 <3825@holbertonschool.com>
2022-04-06 18:22:10 -0500

    add style

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,8 +4,8 @@ exports.esrever = function (j) {
   let i = j.length - 1;
   let a = 0;
   let tmp;
-  while (a < i){
-    tmp = j[a]
+  while (a < i) {
+    tmp = j[a];
     j[a] = j[i];
     j[i] = tmp;
     a++;
