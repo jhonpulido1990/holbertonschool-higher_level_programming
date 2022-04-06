97b0d8f221c98da0cc5f849201091a5e3862a764
jhonpulido1990 <3825@holbertonschool.com>
2022-04-06 16:00:09 -0500

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,47 @@
+#!/usr/bin/node
+let i = 0;
+let j;
+let charact = 'x';
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return undefined;
+    }
+  }
+
+  print () {
+    i = 0;
+    while (i < this.height) {
+      console.log(charact.repeat(this.width));
+      i++;
+    }
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height *2 ;
+  }
+
+  rotate () {
+    j = this.height;
+    this.height = this.width;
+    this.width = j;
+  }
+};
+
+module.exports = class Square extends Rectangle{
+    constructor (size){
+        super(size, size);
+    }
+    charPrint(c){
+        if (c === undefined){
+            this.print();
+        }else{
+            charact = 'C';
+            this.print();
+        }
+    }
+};
