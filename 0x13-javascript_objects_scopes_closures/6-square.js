c5651ec684ebc07550736cba652a362e5940a40b
jhonpulido1990 <3825@holbertonschool.com>
2022-04-06 19:19:42 -0500

    add variable local is X if c is nudefined

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -40,6 +40,7 @@ module.exports = class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
       this.print();
+      charact = 'X'
     } else {
       charact = 'C';
       this.print();
