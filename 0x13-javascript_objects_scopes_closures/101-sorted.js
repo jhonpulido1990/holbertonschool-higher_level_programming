7deb9564fe7cf4160fba38b3a256beccc7ed788c
jhonpulido1990 <3825@holbertonschool.com>
2022-04-07 12:39:27 -0500

    advance 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const dict = require('./101-data');
+
+const diction = dict.dict;
+const arr = [];
+const arr1 = [];
+const arr2 = [];
+const newdict = {};
+for (const key in diction) {
+  const value = diction[key];
+  console.log(key + ':' + value);
+  if (value === 1) {
+    arr.push(key);
+    newdict[value] = arr;
+  } else if (value === 2) {
+    arr1.push(key);
+    newdict[value] = arr1;
+  } else {
+    arr2.push(key);
+    newdict[value] = arr2;
+  }
+}
+
+console.log(newdict);
