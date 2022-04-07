29af0ab40fa938b4df6f6d8951df14e05ad28e63
jhonpulido1990 <3825@holbertonschool.com>
2022-04-07 11:33:40 -0500

    change funtion print

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,37 +1,7 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle')
+
 let i = 0;
-let j;
-let charact = 'X';
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      return undefined;
-    }
-  }
-
-  print () {
-    i = 0;
-    while (i < this.height) {
-      console.log(charact.repeat(this.width));
-      i++;
-    }
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-
-  rotate () {
-    j = this.height;
-    this.height = this.width;
-    this.width = j;
-  }
-}
-
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
@@ -39,11 +9,13 @@ module.exports = class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined) {
-      charact = 'X';
       this.print();
     } else {
-      charact = c;
-      this.print();
+      i = 0;
+      while (i < this.height) {
+      console.log(c.repeat(this.width));
+      i++;
+    }
     }
   }
 };
