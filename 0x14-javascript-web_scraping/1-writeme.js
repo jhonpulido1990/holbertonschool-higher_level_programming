fae080d4a78025f360e41c363deb584af40d1aba
jhonpulido1990 <3825@holbertonschool.com>
2022-04-11 19:17:19 -0500

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const args = process.argv;
+/***
+ * implementation of writeFile
+ */
+
+fs.writeFile(args[2], args[3], 'utf8',function (err) {
+  if (err) return console.error(err);
+});
