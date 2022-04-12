3f8fdee478572af5a90b4987c179860aa41c9e8d
jhonpulido1990 <3825@holbertonschool.com>
2022-04-12 00:16:20 -0500

    style semistandar

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,6 +5,6 @@ const args = process.argv;
  * implementation of writeFile
  */
 
-fs.writeFile(args[2], args[3], 'utf8',function (err) {
+fs.writeFile(args[2], args[3], 'utf8', function (err) {
   if (err) return console.error(err);
 });
