5e4ac8e97fcb6e50c98f5566824f5b495477ebb0
jhonpulido1990 <3825@holbertonschool.com>
2022-04-12 00:30:29 -0500

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+const url = args[2];
+const fs = require('fs');
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  fs.writeFile(args[3], body, 'utf-8', function (error) {
+    console.error(error);
+  });
+});
