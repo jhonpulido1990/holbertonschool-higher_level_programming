88fc947603e87eb70d2ea7a8bdc5b75de1a37999
jhonpulido1990 <3825@holbertonschool.com>
2022-05-10 00:08:42 -0500

    add comment

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Write a script that
+"""
+Write a script that
 lists all states from the
-database hbtn_0e_0_usa"""
+database hbtn_0e_0_usa
+"""
 import MySQLdb
 from sys import argv
 
