62bbdf1b6bbcd391353e7f6770f48b1b45425602
jhonpulido1990 <3825@holbertonschool.com>
2022-05-10 18:03:45 -0500

    add comment

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
+"""
+Write a script that lists all
+State objects from the database
+hbtn_0e_6_usa
+"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
