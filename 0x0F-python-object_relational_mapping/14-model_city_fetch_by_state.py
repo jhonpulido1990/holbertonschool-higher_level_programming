a7a3ce61117f8455346c025ab31adfc6b709b02a
jhonpulido1990 <3825@holbertonschool.com>
2022-05-10 23:55:28 -0500

    task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+Write a script that lists all
+State objects from the database
+hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.schema import Table
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    seccion = session.query(State, City).filter(City.state_id == State.id)\
+                                       .order_by(City.id).all()
+
+    for state, city in seccion:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    session.close()
