307f068970f4bc00b3503e4590f80815115343e4
jhonpulido1990 <3825@holbertonschool.com>
2022-05-18 18:16:18 -0500

    sapce between flag

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A header variable X-HolbertonSchool-User-Id must be sent with the value 98
-curl -sX GET "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s -X GET "$1" -H "X-HolbertonSchool-User-Id: 98"
