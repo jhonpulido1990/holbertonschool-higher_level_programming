4015cc081e437b7b53d30b799212e035100f257b
jhonpulido1990 <3825@holbertonschool.com>
2022-05-18 18:34:34 -0500

    change code

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A variable email must be sent with the value test@gmail.com
 # A variable subject must be sent with the value I will always be here for PLD
-curl -sX POST -d "subject=I will always be here for PLD" -d "email=test@gmail.com" "$1"
+curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
