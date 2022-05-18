992484289c0f1aefb5745ad13af793262bf1e9aa
jhonpulido1990 <3825@holbertonschool.com>
2022-05-18 18:13:30 -0500

    change order get

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A header variable X-HolbertonSchool-User-Id must be sent with the value 98
-curl -H "X-HolbertonSchool-User-Id: 98" -sX GET "$1"
+curl -sX GET "$1" -H "X-HolbertonSchool-User-Id: 98"
