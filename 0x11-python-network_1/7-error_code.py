4fcc4b7369dff052200ce4f0f1c84d441c1c1e46
jhonpulido1990 <3825@holbertonschool.com>
2022-05-19 23:22:57 -0500

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches
+https://intranet.hbtn.io/status
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
